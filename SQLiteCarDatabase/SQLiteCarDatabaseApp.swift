b3448eafd7f423d7d85350e087cb9d3859e2bc06
Hubert <hkrupa96@gmail.com>
2025-12-23 11:21:40 +0100

    Initial Commit

---
+++ SQLiteCarDatabase/SQLiteCarDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SQLiteCarDatabaseApp.swift
+//  SQLiteCarDatabase
+//
+//  Created by Hubert Krupa on 23/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct SQLiteCarDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
